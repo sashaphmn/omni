e835c50ff10420be46679bbda727334882351e85
Kevin Halliday <kevin@omni.network>
2024-10-07 15:09:54 -0400

    chore(contracts/core): add cantina test templates

---
+++ contracts/core/test/templates/OmniPortal.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.24;
+
+/**
+ * @title OmniPortal_Test
+ * @notice Template test suite for src/xchain/OmniPortal.sol
+ * @dev Available fixtures (see OmniPortalFixtures.sol):
+ *
+ *  OmniPortal portal; // the portal contract
+ *
+ */
+contract OmniPortal_Test { }
